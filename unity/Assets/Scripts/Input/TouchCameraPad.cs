77aac02a3c550798e48b8baa587e8a1fb44902f8
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-19 17:17:02 +0300

    Allow deterministic touch camera scene wiring

--- unity/Assets/Scripts/Input/TouchCameraPad.cs
+++ unity/Assets/Scripts/Input/TouchCameraPad.cs
@@ -7,6 +7,11 @@ namespace MMORPG.Input
     {
         [SerializeField] private float sensitivity = 0.12f;
 
+        public void Configure(float value)
+        {
+            sensitivity = Mathf.Max(0.001f, value);
+        }
+
         public void OnDrag(PointerEventData eventData)
         {
             MobileInputState.AddLookDelta(eventData.delta * sensitivity);
