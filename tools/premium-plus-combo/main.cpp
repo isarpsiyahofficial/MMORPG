8be46e6c117936b7066afa5c673283ac463c64b7
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-24 11:01:17 +0300

    Wire master power gate into Rogue application

--- tools/premium-plus-combo/main.cpp
+++ tools/premium-plus-combo/main.cpp
@@ -1,4 +1,18 @@
 #include "app_part1.inc"
+
+#define StartMacro PpcLegacyStartMacro
+#define StopMacro PpcLegacyStopMacro
+#define RefreshRuntimeHotkeys PpcLegacyRefreshRuntimeHotkeys
 #include "app_part2.inc"
+#undef RefreshRuntimeHotkeys
+#undef StopMacro
+#undef StartMacro
+
+#define MakeChildControls PpcLegacyMakeChildControls
+#define DrawUi PpcLegacyDrawUi
 #include "app_part3.inc"
+#undef DrawUi
+#undef MakeChildControls
+
+#include "app_power.inc"
 #include "app_part4.inc"
